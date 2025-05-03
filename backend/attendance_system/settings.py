f468c2c6aac86be84319e7bbcf192943173debe8
Vaibhav <107534334+Vaibhavk18@users.noreply.github.com>
2025-05-04 04:37:41 +0530

    Update settings.py

--- backend/attendance_system/settings.py
+++ backend/attendance_system/settings.py
@@ -35,8 +35,8 @@ AUTH_USER_MODEL = 'attendance_app.CustomUser'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vaibhavvishalvk18@gmail.com'
-EMAIL_HOST_PASSWORD = 'kkpypybjxjgeybkf'
+EMAIL_HOST_USER = 'your-email'
+EMAIL_HOST_PASSWORD = 'your-16-char-app-password'
 EMAIL_USE_TLS = True
 
 
